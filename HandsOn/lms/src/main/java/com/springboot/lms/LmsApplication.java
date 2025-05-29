6d6f08e417976ea7bcefb0d7ca100a00865c595b
SreevathsanKG <113751638+SreevathsanKG@users.noreply.github.com>
2025-05-29 10:51:55 +0530

    initial-security-setup

--- HandsOn/lms/src/main/java/com/springboot/lms/LmsApplication.java
+++ HandsOn/lms/src/main/java/com/springboot/lms/LmsApplication.java
@@ -2,9 +2,9 @@ package com.springboot.lms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
+//@SpringBootApplication(exclude = SecurityAutoConfiguration.class)  //-->> this makes spring to exclude the security
 public class LmsApplication {
 
 	public static void main(String[] args) {
